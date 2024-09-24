baef9e46d0129eec6c026d4b5f8fb9e59af332ce
asjadahamed77 <ahamedasjad77@gmail.com>
2024-09-24 14:51:09 +0530

    Created HomeCategories

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PhoneAdd from '../components/PhoneAdd'
+import HomeCategories from '../components/HomeCategories'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className='bg-slate-50'>
       <PhoneAdd />
+      <div className='flex flex-col justify-center sm:w-[90%] w-full px-10  mx-auto'>
+        <HomeCategories />
+      </div>
     </div>
   )
 }
